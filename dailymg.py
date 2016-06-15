ed19d1577645324372632a8a9095175777f077bb
Romuald Brunet <romuald@chivil.com>
2016-06-15 20:36:28 +0200

    Remove module-level unused code

--- dailymg.py
+++ dailymg.py
@@ -49,9 +49,8 @@ DAYS = 60
 MAX = 5
 POOL_SIZE = 4
 
-# init in main()
-BLACKLIST = None
-DATADIR = None
+ICHARS = ('[=--]', '[-=-]', '[--=]', '[-=-]')
+CLEAR = '\r\033[2K'
 
 
 class Blacklist(object):
@@ -80,119 +79,6 @@ class Blacklist(object):
 
         with file(self.storepath, 'w+') as blfile:
             json.dump(self.list, blfile)
-
-
-def store_photo_url(path, url):
-    """
-    Optionally set the URL metadata for this photo (MacOS only)
-    """
-    try:
-        libc = ctypes.cdll.LoadLibrary('libc.dylib')
-        setxattr = libc.setxattr
-    except (OSError, AttributeError) as err:
-        # print "fail", err
-        return
-
-    path = path.encode('utf-8')
-    url = url.encode('utf-8')
-
-    setxattr(path, b'com.apple.metadata:kMDItemWhereFroms',
-             url, len(url), 0, 0)    
-
-
-def get_metadata(day):
-    """Fetch JSON data for a given day"""
-
-    filename = day.strftime('%Y-%m-%d.json')
-    filepath = os.path.join(DATADIR, 'metadata', filename)
-    zfilepath = filepath + '.gz'
-    if os.path.isfile(zfilepath):
-        with gzip.GzipFile(zfilepath) as cachefile:
-            return json.load(cachefile)
-
-    url = BASE_URL + filename
-    req = urllib2.Request(url)
-    req.add_header('Accept-Encoding', 'gzip')
-    try:
-        res = urllib2.urlopen(req)
-    except Exception as err:
-        # print '%s - %d' % (url, err.code)
-        return None
-
-    if res.info().get('Content-Encoding') == 'gzip':
-        buf = StringIO(res.read())
-        f = gzip.GzipFile(fileobj=buf)
-        data = f.read()
-    else:
-        data = res.read()
-
-    with gzip.GzipFile(zfilepath, 'w+') as cachefile:
-        buf = StringIO(data)
-
-        cachefile.write(data)
-    return json.loads(data)
-
-
-def ratio_ok(photo):
-    return (
-        RATIO - RATIO * RATIO_DELTA <
-        float(photo['ratio']) <
-        RATIO + RATIO * RATIO_DELTA)
-
-
-def store_photo(photo):
-    """Fetch a photo from flickr to storage"""
-    url = photo['url']
-
-    parts = (photo['date'].replace('-', ''),  # day
-             photo['id'], # short id
-             urlparse(url).path.split('.')[-1]) # file extension
-    filename = '%s-%s.%s' % parts
-
-    assert '..' not in filename
-
-    filepath = os.path.join(TARGET, filename)
-    if os.path.isfile(filepath):
-        photo['done'] = True
-        return photo
-
-    # not thread-safe?
-    source = urllib2.urlopen(url)
-    
-    if 'photo_unavailable' in source.url:
-        # pdb.set_trace()
-        BLACKLIST.add(photo)
-        photo['done'] = True
-        return
-
-    contents = source.read()
-
-    destination = open(filepath, 'w+')
-    destination.write(contents)
-    destination.close()
-
-    store_photo_url(filepath, url)
-
-    photo['done'] = True
-
-def remove_expired():
-    # XXX remove old cache files
-    # Do not blindly remove photos of more than X days
-    # Instead, keep N photos. In case we could not retrive some recent photos
-    to_keep = DAYS * MAX  
-    photo_re = re.compile('^[0-9]+-[a-zA-Z0-9]+\.[a-zA-Z0-9]+$')
-    tmp = os.listdir(TARGET)
-    photos = [name for name in tmp if photo_re.match(name)]
-    photos.sort(reverse=True)
-
-    to_remove = photos[to_keep:]
-    for photo in to_remove:
-        os.unlink(os.path.join(TARGET, photo))
-    print "Deleted %d old photos" % len(to_remove)
-
-
-ICHARS = ('[=--]', '[-=-]', '[--=]', '[-=-]')
-CLEAR = '\r\033[2K'
 
 
 def main():
@@ -333,6 +219,7 @@ class Dailymg(object):
         pool = Pool(POOL_SIZE)
 
         todo = [self.start_date - timedelta(days=i) for i in range(self.days)]
+
         metadata = pool.map_async(self.get_metadata, todo)
         while not metadata.ready():
             progress()
@@ -370,7 +257,7 @@ class Dailymg(object):
 
         pool = Pool(POOL_SIZE)
 
-        res = pool.map_async(store_photo, to_fetch)
+        res = pool.map_async(self.store_photo, to_fetch)
 
         while not res.ready():
             progress()
@@ -382,89 +269,6 @@ class Dailymg(object):
 
         self.remove_expired()
 
-
-
-def main():
-    global BLACKLIST, DATADIR
-
-    date = START_DATE
-    to_fetch = []
-    done, todo = 0, DAYS
-    iprogress = cycle(ICHARS)
-
-    def progress():
-        sys.stderr.write(CLEAR + 'Fetching metadata %s' % next(iprogress))
-        sys.stderr.flush()
-
-    DATADIR = os.path.join(TARGET, '.dailymg')
-    if not os.path.isdir(DATADIR):
-        os.mkdir(DATADIR)
-
-    mddir = os.path.join(DATADIR, 'metadata')
-    if not os.path.isdir(mddir):
-        os.mkdir(mddir)
-
-    BLACKLIST = Blacklist()
-    BLACKLIST.load(DATADIR)
-
-    pool = Pool(POOL_SIZE)
-
-    todo = [date - timedelta(days=i) for i in range(DAYS)]
-    metadata = pool.map_async(get_metadata, todo, chunksize=1)
-    while not metadata.ready():
-        progress()
-        sleep(.1)
-    sys.stderr.write(CLEAR + 'Fetching metadata [ * ]\n')
-    sys.stderr.flush()
-
-    for data in metadata.get(): # while False:  # for _ in xrange(DAYS):
-        # date -= timedelta(days=1)
-        # data = get_metadata(date)
-        if data is None:
-            continue
-
-        photos = []
-        for photo in data['photos']:
-            if ratio_ok(photo) and photo not in BLACKLIST:
-                photo['date'] = data['date']
-                photos.append(photo)
-
-        # print "%s %d photos matching %.1f +/-%d%% ratio " % (
-        #     date.strftime('%Y-%m-%d'), len(photos), RATIO, RATIO_DELTA * 100)
-
-        to_fetch.extend(photos[:MAX])
-
-    print >> sys.stderr, ''
-
-    #existing = set(os.listdir(TARGET))
-    #to_fetch = [photo for photo in to_fetch if photo]
-
-
-    print '%d photos to fetch' % len(to_fetch)
-
-    def progress():
-        count = len([_ for _ in to_fetch if 'done' in _])
-        dd = '%%%dd' % len(str(len(to_fetch)))
-        template = 'Fetching %s/%s photos' % (dd, dd)
-        sys.stderr.write('\r' + template % (count, len(to_fetch)))
-        sys.stderr.flush()
-
-    pool = Pool(POOL_SIZE)
-
-    res = pool.map_async(store_photo, to_fetch)
-
-    while not res.ready():
-        progress()
-        sleep(.1)
-    progress()
-    print ''
-
-    BLACKLIST.save()
-
-    remove_expired()
-
-
 if __name__ == '__main__':
-    # main()
     Dailymg(TARGET).start()
 
