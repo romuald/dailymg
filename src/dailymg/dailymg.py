12c7bd6988269993a5baca4b3254e6b519317ba8
Romuald Brunet <romuald@chivil.com>
2016-07-17 16:57:55 +0200

    Externalize photo class, remove cstringio

--- src/dailymg/dailymg.py
+++ src/dailymg/dailymg.py
@@ -15,18 +15,14 @@ import argparse
 from time import sleep
 from pprint import pprint
 from itertools import cycle
-from urlparse import urlparse
+from StringIO import StringIO
 from datetime import datetime, timedelta
 from ConfigParser import SafeConfigParser
-
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
 
 from multiprocessing.dummy import Pool
 
 from .blacklist import Blacklist
+from .photo import Photo
 
 
 CONFIG_TEMPLATE = """
@@ -47,46 +43,6 @@ TARGET = './photos/'
 POOL_SIZE = 4
 ICHARS = ('[=--]', '[-=-]', '[--=]', '[-=-]')
 CLEAR = '\r\033[2K'
-
-
-class Photo(object):
-    def __init__(self, dailymg, photo):
-        self.id = photo['id']
-        self.url = photo['url']
-        self.date = photo.get('date')
-        self.ratio = float(photo['ratio'])
-        self.done = False  # Used for download progress check only
-        self.mg = dailymg
-
-    @property
-    def ratio_ok(self):
-        return (
-            self.mg.ratio - self.mg.ratio * self.mg.ratio_delta <
-            self.ratio <
-            self.mg.ratio + self.mg.ratio * self.mg.ratio_delta)
-
-    @property
-    def filename(self):
-        assert self.date
-
-        url = self.url
-
-        parts = (self.date.replace('-', ''),         # day
-                 self.id,                            # short id
-                 urlparse(url).path.split('.')[-1])  # file extension
-        filename = '%s-%s.%s' % parts
-
-        assert '..' not in filename
-
-        return filename
-
-    @property
-    def path(self):
-        """The local file path"""
-        return os.path.join(self.mg.target, self.filename)
-
-    def __repr__(self):
-        return '<Photo %s (%s)>' % (self.id, self.date)
 
 
 def main():
---
+++ src/dailymg/photo.py
@@ -0,0 +1,42 @@
+import os.path
+from urlparse import urlparse
+
+
+class Photo(object):
+    def __init__(self, dailymg, photo):
+        self.id = photo['id']
+        self.url = photo['url']
+        self.date = photo.get('date')
+        self.ratio = float(photo['ratio'])
+        self.done = False  # Used for download progress check only
+        self.mg = dailymg
+
+    @property
+    def ratio_ok(self):
+        return (
+            self.mg.ratio - self.mg.ratio * self.mg.ratio_delta <
+            self.ratio <
+            self.mg.ratio + self.mg.ratio * self.mg.ratio_delta)
+
+    @property
+    def filename(self):
+        assert self.date
+
+        url = self.url
+
+        parts = (self.date.replace('-', ''),         # day
+                 self.id,                            # short id
+                 urlparse(url).path.split('.')[-1])  # file extension
+        filename = '%s-%s.%s' % parts
+
+        assert '..' not in filename
+
+        return filename
+
+    @property
+    def path(self):
+        """The local file path"""
+        return os.path.join(self.mg.target, self.filename)
+
+    def __repr__(self):
+        return '<Photo %s (%s)>' % (self.id, self.date)
