172c989287cad79ae46659e259161e6d9678b62b
Romuald Brunet <romuald@chivil.com>
2016-11-06 13:47:33 +0100

    Switch pytest + runner

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+import sys
+
 from setuptools import setup, find_packages, Extension
+
 
 NAME = 'dailymg'
 VERSION = '0.1'
 AUTHOR = 'Romuald Brunet'
 
+TEST_REQUIRE = ['pytest', 'mock']
 
 setup(name=NAME,
       version=VERSION,
@@ -12,6 +16,9 @@ setup(name=NAME,
       author='Romuald Brunet',
       author_email='romuald@chivil.com',
       entry_points={'console_scripts': ['dailymg = dailymg:main']},
+      setup_requires=['pytest-runner'],
+      tests_require=TEST_REQUIRE,
+      extras_require={'test': TEST_REQUIRE},
       classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
